e749c61f0ababe4dba24efa095126dcc5e232c9e
gyf-i <gaoyunfan8@mgmail.com>
2022-05-29 00:18:18 +0800

    Create NavBar tried email login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { useAuth } from './hooks/useAuth';
+import Navbar from "./NavBar";
+import PageLogin from './pages/PageLogin';
+import PageTodo from './pages/PageTodo';
 
-export default App;
+export default function App() {
+  const { user } = useAuth();
+  return <div className="App">
+  <Navbar />
+  {user ? <PageTodo /> : <PageLogin />}</div>
+};
--- src/index.js
+++ src/index.js
@@ -3,11 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ProvideAuth } from './hooks/useAuth';
+
+// import `ChakraProvider` component
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+
+
+//Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ProvideAuth>
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
+    </ProvideAuth>
   </React.StrictMode>
 );
 
