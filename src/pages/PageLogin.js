2e566198f299a8868711772570b388ca0197db48
gyf-i <gaoyunfan8@mgmail.com>
2022-05-29 17:29:29 +0800

    Create new login

--- src/pages/PageLogin.js
+++ src/pages/PageLogin.js
@@ -1,19 +1,35 @@
 import { useAuth } from "../hooks/useAuth";
+import OAuthButtonGroup from "./OAuthBtoonGroup";
+
 import {
   Box,
-  VStack,
   Button,
-  ButtonGroup,
-  Input,
+  Checkbox,
+  Container,
+  Divider,
   FormControl,
   FormLabel,
-  FormErrorMessage,
+  Heading,
+  HStack,
+  Input,
+  Stack,
+  Text,
+  useBreakpointValue,
+  useColorModeValue,
+  InputGroup,
+  InputRightElement,
+  IconButton,
   FormHelperText,
+  FormErrorMessage,
+  ButtonGroup,
 } from "@chakra-ui/react";
 
 import { useForm } from "react-hook-form";
 
-function PageLogin() {
+import { useState } from "react";
+import { HiEye, HiEyeOff } from "react-icons/hi";
+
+export default function PageLogin() {
   const {
     user,
     signin,
@@ -21,8 +37,8 @@ function PageLogin() {
     signout,
     sendPasswordResetEmail,
     confirmPasswordReset,
-    signInWithGoogle,
   } = useAuth();
+
   const {
     register,
     handleSubmit,
@@ -31,43 +47,118 @@ function PageLogin() {
 
   const onSubmit = (data) => {
     console.log(data);
-    {signin(data.email, data.password)}
-  }
+      signin(data.email, data.password);
+  };
+  const [isOpen, setOpen] = useState(false);
+  const onClickReveal = () => setOpen(!isOpen);
 
   return (
-    <Box margin="100px">
-      <h1>Login</h1>
-      <Button type="button" onClick={signInWithGoogle}>
-        Sign in with Google
-      </Button>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <VStack>
-          <Input
-            type="email"
-            plailceholder="email"
-            {...register("email", { required: "Please enter Email" })}
-          />
-          <Input
-            type="password"
-            placeholder="passoword"
-            {...register("password", {
-              required: "Please enter Password",
-              minLength: { value: 8, message: "Too short" },
-            })}
-          />
-          <Button
-            borderRadius="md"
-            bg="cyan.600"
-            _hover={{ bg: "cyan.200" }}
-            variant="ghost"
-            type="submit"
+    <Container
+      maxW="lg"
+      py={{
+        base: "12",
+        md: "24",
+      }}
+      px={{
+        base: "0",
+        sm: "8",
+      }}
+    >
+      <Stack spacing="8">
+        <Stack spacing="6">
+          {/*<Logo />*/}
+          <Stack
+            spacing={{
+              base: "2",
+              md: "3",
+            }}
+            textAlign="center"
           >
-            Login
-          </Button>
-        </VStack>
-      </form>
-    </Box>
+            <Heading
+              size={useBreakpointValue({
+                base: "xs",
+                md: "sm",
+              })}
+            >
+              Log in to your account
+            </Heading>
+            <HStack spacing="1" justify="center">
+              <Text color="muted">Don't have an account?</Text>
+              <Button variant="link" colorScheme="blue">
+                Sign up
+              </Button>
+            </HStack>
+          </Stack>
+        </Stack>
+        <Box
+          py={{
+            base: "0",
+            sm: "8",
+          }}
+          px={{
+            base: "4",
+            sm: "10",
+          }}
+          bg={useBreakpointValue({
+            base: "transparent",
+            sm: "bg-surface",
+          })}
+          boxShadow={{
+            base: "none",
+            sm: useColorModeValue("md", "md-dark"),
+          }}
+          borderRadius={{
+            base: "none",
+            sm: "xl",
+          }}
+        >
+          <Stack spacing="6">
+            <Stack spacing="5">
+              <FormControl>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input id="email" type="email" />
+              </FormControl>
+              {/*<PasswordField />*/}
+              <FormControl>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <InputGroup>
+                  <InputRightElement>
+                    <IconButton
+                      variant="link"
+                      aria-label={isOpen ? "Mask password" : "Reveal password"}
+                      icon={isOpen ? <HiEyeOff /> : <HiEye />}
+                      onClick={onClickReveal}
+                    />
+                  </InputRightElement>
+                  <Input
+                    id="password"
+                    name="password"
+                    type={isOpen ? "text" : "password"}
+                    required
+                  />
+                </InputGroup>
+              </FormControl>
+            </Stack>
+            <HStack justify="space-between">
+              <Checkbox defaultChecked>Remember me</Checkbox>
+              <Button variant="link" colorScheme="blue" size="sm">
+                Forgot password?
+              </Button>
+            </HStack>
+            <Stack spacing="6">
+              <Button colorScheme='twitter' variant="solid">Sign in</Button>
+              <HStack>
+                <Divider />
+                <Text fontSize="sm" whiteSpace="nowrap" color="muted">
+                  or continue with
+                </Text>
+                <Divider />
+              </HStack>
+              <OAuthButtonGroup />
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Container>
   );
 }
-
-export default PageLogin;
