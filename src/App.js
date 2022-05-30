0fe772d08f51a332f0dd350beb4da4a19b0d3632
HTT <49441540+HT-T@users.noreply.github.com>
2022-05-30 13:34:52 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import { useAuth } from "./hooks/useAuth";
 import Navbar from "./NavBar";
 import PageLogin from "./pages/PageLogin";
 import PageTodo from "./pages/PageTodo";
+import PageSignup from "./pages/PageSignup";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
   const { user } = useAuth();
 
   return (
+
+    
     <div className="app">
       <Navbar />
       {user ? <PageTodo /> : <PageLogin />}
