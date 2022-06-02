353222de5ff577481fa72715bbb279802ef438c2
gyf-i <gaoyunfan8@mgmail.com>
2022-06-02 15:11:18 +0800

    Create link for modules & studytgt

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PageLogin from "./pages/PageLogin";
 import PageTodo from "./pages/PageTodo";
 import PageSignup from "./pages/PageSignup";
 import PageReset from "./pages/PageReset";
+import Modules from "./components/Modules";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
@@ -12,12 +13,12 @@ export default function App() {
   return (
 
     <div className="app">
-      <Navbar />
     <Router>
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<PageLogin />} />
         <Route exact path="/register" element={<PageSignup />} />
-
+        <Route exact path="/modules" element={<Modules />} />
         <Route exact path="/resetPassword" element={<PageReset />} />
         <Route exact path="/dashboard" element={<PageTodo />} />
       </Routes>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,20 @@
 import {
   Avatar,
-  AVatarBadge,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
-  MenuItemOption,
   MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
-  Text,
   Flex,
   Box,
   Spacer,
   Heading,
   Button,
-  ButtonGroup,
 } from "@chakra-ui/react";
-
-import { Link } from "react-router-dom";
 import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 
-import { useAuth } from "./hooks/useAuth";
+import { Link } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 import { useState } from "react";
 
 export default function Navbar() {
@@ -51,33 +44,31 @@ export default function Navbar() {
       bg="teal.200"
       padding={2}
     >
+
       <Box p="2">
-        <Heading size="md">
-          <Link to="/">StudyTogether</Link>
-        </Heading>
+        <Heading size="md">StudyTogether</Heading>
       </Box>
-      <Button colorScheme="teal" variant="ghost">
+        <Button colorScheme="teal" variant="ghost">
         <Link to="/modules">Modules</Link>
-      </Button>
+        </Button>
       {user && (
         <>
-          <Button colorScheme="teal" variant="ghost">
-            Friends
-          </Button>
-          <Spacer />
-          <Menu>
-            <MenuButton mr={5}>
-              <Avatar size="sm" />
-            </MenuButton>
-            <MenuList>
-              <MenuGroup title="Profile">
-                <MenuItem>My Account</MenuItem>
-                <MenuItem>Settings</MenuItem>
-                <MenuItem onClick={() => signout()}>Sign out</MenuItem>
-              </MenuGroup>
-            </MenuList>
-          </Menu>
-        </>
+        <Button colorScheme="teal" variant="ghost">
+         Friends 
+        </Button>
+      <Spacer />
+        <Menu>
+          <MenuButton mr={5}>
+            <Avatar size="sm" />
+          </MenuButton>
+          <MenuList>
+            <MenuGroup title="Profile">
+              <MenuItem>My Account</MenuItem>
+              <MenuItem>Settings</MenuItem>
+              <MenuItem onClick={() => signout()}>Sign out</MenuItem>
+            </MenuGroup>
+          </MenuList>
+        </Menu></>
       )}
     </Flex>
   );
