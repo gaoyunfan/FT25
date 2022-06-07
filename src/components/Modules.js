7f36f0aa3679cc9481b28db06d2e8da7579cd8f9
gyf-i <gaoyunfan8@mgmail.com>
2022-06-07 21:59:58 +0800

    Add get/save mod data

--- src/components/Modules.js
+++ src/components/Modules.js
@@ -1,30 +1,29 @@
-
-  import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import { db, useAuth } from "../hooks/useAuth";
-import TaskManager from "./TaskManager";
+import ModManager from "./ModManager";
 
 export default function Modules() {
   // Task state has to be lifted to be at the App level
   // because Header also needs to know the task state to display
-  // no. of undone tasks. It cannot be contained within TaskManager
+  // no. of undone mods. It cannot be contained within TaskManager
   // as child components cannot pass props to their parent components.
-  const [tasks, setTasksState] = useState([]);
+  const [mods, setModsState] = useState([]);
 
   const { user } = useAuth();
 
-  function setTasks(newTasks) {
-    setTasksState(newTasks);
-    setDoc(doc(db, "tasks", user?.uid), { tasks: newTasks });
+  function setMods(newMods) {
+    setModsState(newMods);
+    setDoc(doc(db, "mods", "common"), { mods: newMods });
   }
 
   useEffect(() => {
     async function fetchData() {
-      const docSnapshot = await getDoc(doc(db, "tasks", user?.uid));
+      const docSnapshot = await getDoc(doc(db, "mods", "common"));
       if (docSnapshot.exists()) {
-        setTasksState(docSnapshot.data().tasks);
+        setModsState(docSnapshot.data().mods);
       } else {
-        setTasksState([]);
+        setModsState([]);
       }
     }
     fetchData();
@@ -33,7 +32,7 @@ export default function Modules() {
   return (
     <>
       <main>
-        <TaskManager tasks={tasks} setTasks={setTasks} />
+        <ModManager mods={mods} setMods={setMods} />
       </main>
     </>
   ); 
