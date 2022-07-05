f4e2e99b89855f920eb874e6954bec2446226ea9
gyf-i <gaoyunfan8@mgmail.com>
2022-07-05 22:43:25 +0800

    Test with folder name

--- temp/src/App.js
+++ temp/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import React from "react";
+import React , { Component }  from "react";
 
 import Navbar from "./NavBar";
 import PrivateRoute from "./components/user/PrivateRoute";
