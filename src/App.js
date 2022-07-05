209edd80f5b4039a8711f5ba370bab788eef0404
gyf-i <gaoyunfan8@mgmail.com>
2022-07-05 17:42:14 +0800

    Reedit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import Navbar from "./NavBar";
-import PageLogin from "./pages/PageLogin";
-import PageRoom from "./components/Rooms/PageRoom";
-import PageSignup from "./pages/PageSignup";
-import PageReset from "./pages/PageReset";
-import Modules from "./components/modules/Modules";
-import Friends from "./components/NavBar/Friends";
-import FocusRoom from "./components/Rooms/focusRoom";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React, { Component }  from "react";
 import Stopwatch from "./components/Timer/Stopwatch";
@@ -20,14 +14,7 @@ export default function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<PageLogin />} />
-          <Route exact path="/register" element={<PageSignup />} />
-          <Route exact path="/modules" element={<Modules />} />
-          <Route exact path="/resetPassword" element={<PageReset />} />
-          <Route exact path="/dashboard" element={<PageRoom />} />
-          <Route exact path="/focusroom" element={<FocusRoom />} />
-          <Route exact path="/friends" element={<Friends />} />
-          <Route exact path="/timer" element={<><Stopwatch/><Countdown/></>}   />
+          <Route exact path="/" element={<MainPage />} />
           <Route
             path="*"
             element={
