727f1e10195ceae61b203e818a878288c20c3c73
gyf-i <gaoyunfan8@mgmail.com>
2022-07-16 18:27:13 +0800

    Add cancel friend request

--- src/components/rooms/RoomInfo.js
+++ src/components/rooms/RoomInfo.js
@@ -1,4 +1,3 @@
-import { AddIcon } from "@chakra-ui/icons";
 import {
   Modal,
   ModalOverlay,
@@ -15,18 +14,70 @@ import {
   IconButton,
   Text,
   Button,
+  Toast,
 } from "@chakra-ui/react";
-import { doc } from "firebase/firestore";
-import { useState } from "react";
-import { useDocument } from "react-firebase-hooks/firestore";
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { useDocumentData } from "react-firebase-hooks/firestore";
 import { useAuth } from "../../hooks/useAuth";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import { MdCancel } from "react-icons/md";
+import { TbFriends } from "react-icons/tb";
+import { useEffect, useState } from "react";
 
 export default function RoomInfo(props) {
   const { user, db } = useAuth();
   const { room, members_list } = props;
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [friends_list] = useDocument(doc(db, "friends", user.uid));
+  const [friends_list] = useDocumentData(doc(db, "friends", user.uid));
+  const [isAdmin, setIsAdmin] = useState(false);
+  console.log("friends_list", friends_list);
   console.log("friends", friends_list?.friends);
+  console.log("sendRequest", friends_list?.sendRequest);
+
+  useEffect(() => {
+    setIsAdmin(false);
+    if (room?.admin.includes(user?.uid)) {
+      setIsAdmin(true);
+    }
+  }, [room, user?.uid]);
+  console.log("admin = ", isAdmin);
+
+
+  const handleAddFriends = async (u) => {
+    const ref = doc(db, "friends", u.uid);
+    console.log("u", u.uid);
+    await updateDoc(ref, {
+      friendRequest: arrayUnion(user.uid),
+    });
+    await updateDoc(doc(db, "friends", user.uid), {
+      sendRequest: arrayUnion(u.uid),
+    });
+    Toast({
+      title: "Request submitted!",
+      status: "success",
+      duration: 5000,
+      isClosable: true,
+      position: "bottom",
+    });
+  };
+
+  const handleCancelRequest = async (u) => {
+
+    const ref = doc(db, "friends", u.uid);
+    await updateDoc(ref, {
+      friendRequest: arrayRemove(user.uid),
+    });
+    await updateDoc(doc(db, "friends", user.uid), {
+      sendRequest: arrayRemove(u.uid),
+    });
+    Toast({
+      title: "Request cancelled!",
+      status: "success",
+      duration: 5000,
+      isClosable: true,
+      position: "bottom",
+    });
+  }
 
   return (
     <>
@@ -39,7 +90,13 @@ export default function RoomInfo(props) {
         <Avatar size="sm" src="" marginEnd={3} />
         <Heading size="lg">{room?.name} </Heading>
       </Flex>
-      <Modal onClose={onClose} size="lg" isOpen={isOpen} closeOnOverlayClick={false} isCentered>
+      <Modal
+        onClose={onClose}
+        size="lg"
+        isOpen={isOpen}
+        closeOnOverlayClick={false}
+        isCentered
+      >
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Room members</ModalHeader>
@@ -48,13 +105,17 @@ export default function RoomInfo(props) {
             <Flex flexDirection="column" overflowY="auto">
               {members_list?.map((u, key) => {
                 let isFriend = false;
+                let isRequestSend = false;
+                let isUser = false; 
                 let b_g = "#E8E8E8";
                 if (u.uid === user.uid) {
-                  isFriend = true;
+                  isUser = true;
                   b_g = "#38B2AC";
                 }
                 if (friends_list?.friends?.includes(u.uid)) {
                   isFriend = true;
+                } else if (friends_list?.sendRequest?.includes(u.uid)) {
+                  isRequestSend = true;
                 }
                 return (
                   <Flex
@@ -76,15 +137,31 @@ export default function RoomInfo(props) {
                         {u.email}
                       </Text>
                     </Box>
-                    {!isFriend && (
-                      <IconButton
+                    {isUser ? "" : (isFriend ? 
+                    <IconButton
                         ml="auto"
-                        variant="unstyled"
+                        variant="outline"
                         size="lg"
                         aria-label="Add to friends"
-                        icon={<AddIcon />}
+                        icon={<TbFriends />}
                       />
-                    )}
+                    : (isRequestSend ? 
+                      <IconButton
+                        ml="auto"
+                        variant="outline"
+                        size="lg"
+                        aria-label="Add to friends"
+                        onClick={() => handleCancelRequest(u)}
+                        icon={<MdCancel />} /> : 
+                      <IconButton
+                        ml="auto"
+                        variant="outline"
+                        size="lg"
+                        aria-label="Add to friends"
+                        onClick={() => handleAddFriends(u)}
+                        icon={<AiOutlineUserAdd />}
+                      />
+                     ))}
                   </Flex>
                 );
               })}
